4e3d83da3b484ef4194514651cc096214e20852e
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-07-26 10:57:21 +0530

    admin has built and registered

--- MaakCollection/account/admin.py
+++ MaakCollection/account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from account.models import Topic, Webpage
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(Webpage)
--- MaakCollection/account/models.py
+++ MaakCollection/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+class Webpage(models.Model):
+    category = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name = models.CharField(max_length=264)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
